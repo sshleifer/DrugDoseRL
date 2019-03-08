6a7ed5871d848e9de43703cf7c28c45aee7e3f66
Jennie Chen <jchen437@berkeley.edu>
2019-03-08 11:58:54 -0800

    adding some features to linucb + minor changes

--- linucb.py
+++ linucb.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+from tqdm import tqdm
 from get_features import get_features
 from utils import dose2str, get_sample_order
 
@@ -22,7 +23,6 @@ class LinUCB:
             p = np.dot(theta, features)
             p += self.alpha * math.sqrt(np.dot(features, np.dot(np.linalg.inv(self.A_lst[i]), features)))
             p_vals.append(p)
-        # print("p-vals:", p_vals)
         return np.argmax(p_vals)
 
     def update(self, chosen_arm, reward, features):
@@ -40,20 +40,30 @@ def main():
     arms = ["low", "medium", "high"]
     X, y = get_features()
     X['bias'] = 1
-    # features to use
     feature_select = [
         'Age',
         'Weight (kg)',
         'Height (cm)',
+        'VKORC1 genotype: 1173 C>T(6484); chr16:31012379; rs9934438; A/G_C/C',
+        'CYP2C9 consensus_*1/*2',
+        'VKORC1 -1639 consensus_G/G',
+        'VKORC1 1542 consensus_G/G',
+        'Valve Replacement',
+        'VKORC1 genotype: 1542G>C (6853); chr16:31012010; rs8050894; C/G_G/G',
+        'VKORC1 genotype: -1639 G>A (3673); chr16:31015190; rs9923231; C/T_G/G',
+        'Race_White',
+        'Current Smoker',
+        'VKORC1 1173 consensus_C/C',
+        'Ethnicity_not Hispanic or Latino',
+        'Diabetes',
+        'Congestive Heart Failure and/or Cardiomyopathy',
         'bias'
     ]
-    X_subset = X.loc[:, feature_select]
+    X_subset = X[feature_select]
     num_features = len(feature_select)
     linucb = LinUCB(num_features, X_subset.shape[0])
     total_regret = 0
-    for i in range(X_subset.shape[0]):
-        if i % 50 == 0:
-            print("Processed %s samples" % i)
+    for i in tqdm(range(X_subset.shape[0])):
         row_num = linucb.order[i]
         features = np.array(X_subset.iloc[row_num])
         arm = linucb.select_arm(features)
@@ -65,8 +75,10 @@ def main():
             log.write("Chose arm %s with reward %s\n" % (arms[arm], reward))
             log.write("Updated parameters for arm:\n A = %s \n b = %s\n" % (linucb.A_lst[arm], linucb.b_lst[arm]))
 
+    results = open("linucb_results.txt", "a+")
     print("Total regret: %s" % total_regret)
     print("Overall accuracy: %s" % (1 - total_regret / X.shape[0]))
+    results.write("Regret: %s, Accuracy: %s\n" % (total_regret, 1 - total_regret / X.shape[0]))
 
 if __name__ == "__main__":
     main()
